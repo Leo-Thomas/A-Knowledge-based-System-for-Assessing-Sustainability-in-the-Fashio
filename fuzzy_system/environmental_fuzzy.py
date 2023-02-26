7416ffcb7b60508a069c1587cb933fa25910a748
mikebermeo99 <mikeadawsom@gmail.com>
2023-02-26 16:58:19 -0500

    System

---
+++ fuzzy_system/environmental_fuzzy.py
@@ -0,0 +1,242 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+
+class Environmental_fuzzy:
+
+    def __init__(self, musage,rmusage,wusage,eusage,emission,waste):
+        self.range=np.arange(1,8,1)
+        self.poor=[1,1,4]
+        self.average=[3,4,5]
+        self.good=[4,7,7]
+        self.material_usage_value = musage
+        self.recicled_material_value = rmusage
+        self.water_usage_value = wusage
+        self.energy_usage_value = eusage
+        self.emissions_value = emission
+        self.waste_value = waste
+
+    def antecedent(self):
+        self.material_usage=ctrl.Antecedent(self.range, 'Material usage')
+        self.recicled_material_usage=ctrl.Antecedent(self.range, 'Recicled material usage')
+        self.water_usage=ctrl.Antecedent(self.range, 'Water usage')
+        self.energy_usage=ctrl.Antecedent(self.range, 'Energy usage')
+        self.emissions=ctrl.Antecedent(self.range, 'Emissions')
+        self.waste=ctrl.Antecedent(self.range, 'Waste')
+        self.fis1_env_out = ctrl.Antecedent(self.range, 'Fis 1 env out')
+        self.fis2_env_out = ctrl.Antecedent(self.range, 'Fis 2 env out')
+        self.fis3_env_out = ctrl.Antecedent(self.range, 'Fis 3 env out')
+        self.fis4_env_out = ctrl.Antecedent(self.range, 'Fis 4 env out')
+
+        ##-----Membership Functions
+        #FIS1
+        self.material_usage['poor'] = fuzz.trimf(self.material_usage.universe, self.poor)
+        self.material_usage['average'] = fuzz.trimf(self.material_usage.universe, self.average)
+        self.material_usage['good'] = fuzz.trimf(self.material_usage.universe, self.good)
+
+        self.recicled_material_usage['poor'] = fuzz.trimf(self.recicled_material_usage.universe, self.poor)
+        self.recicled_material_usage['average'] = fuzz.trimf(self.recicled_material_usage.universe, self.average)
+        self.recicled_material_usage['good'] = fuzz.trimf(self.recicled_material_usage.universe, self.good)
+
+        #FIS2
+        self.water_usage['poor'] = fuzz.trimf(self.water_usage.universe, self.poor)
+        self.water_usage['average'] = fuzz.trimf(self.water_usage.universe, self.average)
+        self.water_usage['good'] = fuzz.trimf(self.water_usage.universe, self.good)
+
+        self.energy_usage['poor'] = fuzz.trimf(self.energy_usage.universe, self.poor)
+        self.energy_usage['average'] = fuzz.trimf(self.energy_usage.universe, self.average)
+        self.energy_usage['good'] = fuzz.trimf(self.energy_usage.universe, self.good)
+        #FIS3
+        self.emissions['poor'] = fuzz.trimf(self.emissions.universe, self.poor)
+        self.emissions['average'] = fuzz.trimf(self.emissions.universe, self.average)
+        self.emissions['good'] = fuzz.trimf(self.emissions.universe, self.good)
+
+        self.waste['poor'] = fuzz.trimf(self.waste.universe, self.poor)
+        self.waste['average'] = fuzz.trimf(self.waste.universe, self.average)
+        self.waste['good'] = fuzz.trimf(self.waste.universe, self.good)
+        #FIS4
+        self.fis1_env_out['poor'] = fuzz.trimf(self.fis1_env_out.universe, self.poor)
+        self.fis1_env_out['average'] = fuzz.trimf(self.fis1_env_out.universe, self.average)
+        self.fis1_env_out['good'] = fuzz.trimf(self.fis1_env_out.universe, self.good)
+
+        self.fis2_env_out['poor'] = fuzz.trimf(self.fis2_env_out.universe, self.poor)
+        self.fis2_env_out['average'] = fuzz.trimf(self.fis2_env_out.universe, self.average)
+        self.fis2_env_out['good'] = fuzz.trimf(self.fis2_env_out.universe, self.good)
+        #FIS5
+        self.fis3_env_out['poor'] = fuzz.trimf(self.fis3_env_out.universe, self.poor)
+        self.fis3_env_out['average'] = fuzz.trimf(self.fis3_env_out.universe, self.average)
+        self.fis3_env_out['good'] = fuzz.trimf(self.fis3_env_out.universe, self.good)
+
+        self.fis4_env_out['poor'] = fuzz.trimf(self.fis4_env_out.universe, self.poor)
+        self.fis4_env_out['average'] = fuzz.trimf(self.fis4_env_out.universe, self.average)
+        self.fis4_env_out['good'] = fuzz.trimf(self.fis4_env_out.universe, self.good)
+
+    def consequent(self):
+        #FIS1_ENV
+        self.fis1_env= ctrl.Consequent(self.range,'FIS1_ENV')
+        self.fis1_env['poor'] = fuzz.trimf(self.fis1_env.universe, self.poor)
+        self.fis1_env['average'] = fuzz.trimf(self.fis1_env.universe, self.average)
+        self.fis1_env['good'] = fuzz.trimf(self.fis1_env.universe, self.good)
+
+        #FIS2_ENV
+        self.fis2_env = ctrl.Consequent(self.range,'FIS2_ENV')
+        self.fis2_env['poor'] = fuzz.trimf(self.fis2_env.universe, self.poor)
+        self.fis2_env['average'] = fuzz.trimf(self.fis2_env.universe, self.average)
+        self.fis2_env['good'] = fuzz.trimf(self.fis2_env.universe, self.good)
+
+        #FIS3_ENV
+        self.fis3_env = ctrl.Consequent(self.range,'FIS3_ENV')
+        self.fis3_env['poor'] = fuzz.trimf(self.fis3_env.universe, self.poor)
+        self.fis3_env['average'] = fuzz.trimf(self.fis3_env.universe, self.average)
+        self.fis3_env['good'] = fuzz.trimf(self.fis3_env.universe, self.good)
+
+        #FIS4_ENV
+        self.fis4_env = ctrl.Consequent(self.range,'FIS4_ENV')
+        self.fis4_env['poor'] = fuzz.trimf(self.fis4_env.universe, self.poor)
+        self.fis4_env['average'] = fuzz.trimf(self.fis4_env.universe, self.average)
+        self.fis4_env['good'] = fuzz.trimf(self.fis4_env.universe, self.good)
+
+        #FIS4_ENV
+        self.fis5_env = ctrl.Consequent(self.range,'FIS5_ENV')
+        self.fis5_env['poor'] = fuzz.trimf(self.fis5_env.universe, self.poor)
+        self.fis5_env['average'] = fuzz.trimf(self.fis5_env.universe, self.average)
+        self.fis5_env['good'] = fuzz.trimf(self.fis5_env.universe, self.good)
+    
+    def rules(self):
+        #FIS1_ENV
+        rule1fis1_env = ctrl.Rule(self.material_usage['poor'] & self.recicled_material_usage['poor'], self.fis1_env['poor'])
+        rule2fis1_env = ctrl.Rule(self.material_usage['average'] & self.recicled_material_usage['poor'], self.fis1_env['poor'])
+        rule3fis1_env = ctrl.Rule(self.material_usage['good'] & self.recicled_material_usage['poor'], self.fis1_env['average'])
+        rule4fis1_env = ctrl.Rule(self.material_usage['poor'] & self.recicled_material_usage['average'], self.fis1_env['poor'])
+        rule5fis1_env = ctrl.Rule(self.material_usage['average'] & self.recicled_material_usage['average'], self.fis1_env['average'])
+        rule6fis1_env = ctrl.Rule(self.material_usage['good'] & self.recicled_material_usage['average'], self.fis1_env['average'])
+        rule7fis1_env = ctrl.Rule(self.material_usage['poor'] & self.recicled_material_usage['good'], self.fis1_env['average'])
+        rule8fis1_env = ctrl.Rule(self.material_usage['average'] & self.recicled_material_usage['good'], self.fis1_env['average'])
+        rule9fis1_env = ctrl.Rule(self.material_usage['good'] & self.recicled_material_usage['good'], self.fis1_env['good'])
+
+        #FIS2_ENV
+        rule1fis2_env = ctrl.Rule(self.water_usage['poor'] & self.energy_usage['poor'], self.fis2_env['poor'])
+        rule2fis2_env = ctrl.Rule(self.water_usage['average'] & self.energy_usage['poor'], self.fis2_env['poor'])
+        rule3fis2_env = ctrl.Rule(self.water_usage['good'] & self.energy_usage['poor'], self.fis2_env['average'])
+        rule4fis2_env = ctrl.Rule(self.water_usage['poor'] & self.energy_usage['average'], self.fis2_env['poor'])
+        rule5fis2_env = ctrl.Rule(self.water_usage['average'] & self.energy_usage['average'], self.fis2_env['average'])
+        rule6fis2_env = ctrl.Rule(self.water_usage['good'] & self.energy_usage['average'], self.fis2_env['average'])
+        rule7fis2_env = ctrl.Rule(self.water_usage['poor'] & self.energy_usage['good'], self.fis2_env['average'])
+        rule8fis2_env = ctrl.Rule(self.water_usage['average'] & self.energy_usage['good'], self.fis2_env['average'])
+        rule9fis2_env = ctrl.Rule(self.water_usage['good'] & self.energy_usage['good'], self.fis2_env['good'])
+
+        #FIS3_ENV = FIS2_ENV RESULT
+        rule1fis3_env = ctrl.Rule(self.waste['poor'] & self.emissions['poor'], self.fis3_env['poor'])
+        rule2fis3_env = ctrl.Rule(self.waste['average'] & self.emissions['poor'], self.fis3_env['poor'])
+        rule3fis3_env = ctrl.Rule(self.waste['good'] & self.emissions['poor'], self.fis3_env['average'])
+        rule4fis3_env = ctrl.Rule(self.waste['poor'] & self.emissions['average'], self.fis3_env['poor'])
+        rule5fis3_env = ctrl.Rule(self.waste['average'] & self.emissions['average'], self.fis3_env['average'])
+        rule6fis3_env = ctrl.Rule(self.waste['good'] & self.emissions['average'], self.fis3_env['average'])
+        rule7fis3_env = ctrl.Rule(self.waste['poor'] & self.emissions['good'], self.fis3_env['average'])
+        rule8fis3_env = ctrl.Rule(self.waste['average'] & self.emissions['good'], self.fis3_env['average'])
+        rule9fis3_env = ctrl.Rule(self.waste['good'] & self.emissions['good'], self.fis3_env['good'])
+
+        #FIS4_ENV = FIS1_ENV + FIS2_ENV
+        rule1fis4_env = ctrl.Rule(self.fis1_env_out['poor'] & self.fis2_env_out['poor'], self.fis4_env['poor'])
+        rule2fis4_env = ctrl.Rule(self.fis1_env_out['average'] & self.fis2_env_out['poor'], self.fis4_env['poor'])
+        rule3fis4_env = ctrl.Rule(self.fis1_env_out['good'] & self.fis2_env_out['poor'], self.fis4_env['average'])
+        rule4fis4_env = ctrl.Rule(self.fis1_env_out['poor'] & self.fis2_env_out['average'], self.fis4_env['poor'])
+        rule5fis4_env = ctrl.Rule(self.fis1_env_out['average'] & self.fis2_env_out['average'], self.fis4_env['average'])
+        rule6fis4_env = ctrl.Rule(self.fis1_env_out['good'] & self.fis2_env_out['average'], self.fis4_env['average'])
+        rule7fis4_env = ctrl.Rule(self.fis1_env_out['poor'] & self.fis2_env_out['good'], self.fis4_env['average'])
+        rule8fis4_env = ctrl.Rule(self.fis1_env_out['average'] & self.fis2_env_out['good'], self.fis4_env['average'])
+        rule9fis4_env = ctrl.Rule(self.fis1_env_out['good'] & self.fis2_env_out['good'], self.fis4_env['good'])
+
+
+        #FIS5_ENV = FIS4_ENV + FIS3_ENV
+        rule1fis5_env = ctrl.Rule(self.fis4_env_out['poor'] & self.fis3_env_out['poor'], self.fis5_env['poor'])
+        rule2fis5_env = ctrl.Rule(self.fis4_env_out['average'] & self.fis3_env_out['poor'], self.fis5_env['poor'])
+        rule3fis5_env = ctrl.Rule(self.fis4_env_out['good'] & self.fis3_env_out['poor'], self.fis5_env['average'])
+        rule4fis5_env = ctrl.Rule(self.fis4_env_out['poor'] & self.fis3_env_out['average'], self.fis5_env['poor'])
+        rule5fis5_env = ctrl.Rule(self.fis4_env_out['average'] & self.fis3_env_out['average'], self.fis5_env['average'])
+        rule6fis5_env = ctrl.Rule(self.fis4_env_out['good'] & self.fis3_env_out['average'], self.fis5_env['average'])
+        rule7fis5_env = ctrl.Rule(self.fis4_env_out['poor'] & self.fis3_env_out['good'], self.fis5_env['average'])
+        rule8fis5_env = ctrl.Rule(self.fis4_env_out['average'] & self.fis3_env_out['good'], self.fis5_env['average'])
+        rule9fis5_env = ctrl.Rule(self.fis4_env_out['good'] & self.fis3_env_out['good'], self.fis5_env['good'])
+
+        fis1_env_ctrl = ctrl.ControlSystem([rule1fis1_env,rule2fis1_env,rule3fis1_env,rule4fis1_env,rule5fis1_env,rule6fis1_env,rule7fis1_env,rule8fis1_env,rule9fis1_env])
+        fis2_env_ctrl = ctrl.ControlSystem([rule1fis2_env,rule2fis2_env,rule3fis2_env,rule4fis2_env,rule5fis2_env,rule6fis2_env,rule7fis2_env,rule8fis2_env,rule9fis2_env])
+        fis3_env_ctrl = ctrl.ControlSystem([rule1fis3_env,rule2fis3_env,rule3fis3_env,rule4fis3_env,rule5fis3_env,rule6fis3_env,rule7fis3_env,rule8fis3_env,rule9fis3_env])
+        fis4_env_ctrl = ctrl.ControlSystem([rule1fis4_env,rule2fis4_env,rule3fis4_env,rule4fis4_env,rule5fis4_env,rule6fis4_env,rule7fis4_env,rule8fis4_env,rule9fis4_env])
+        fis5_env_ctrl = ctrl.ControlSystem([rule1fis5_env,rule2fis5_env,rule3fis5_env,rule4fis5_env,rule5fis5_env,rule6fis5_env,rule7fis5_env,rule8fis5_env,rule9fis5_env])
+
+        self.fissing1_env = ctrl.ControlSystemSimulation(fis1_env_ctrl)
+        self.fissing2_env = ctrl.ControlSystemSimulation(fis2_env_ctrl)
+        self.fissing3_env = ctrl.ControlSystemSimulation(fis3_env_ctrl)
+        self.fissing4_env = ctrl.ControlSystemSimulation(fis4_env_ctrl)
+        self.fissing5_env = ctrl.ControlSystemSimulation(fis5_env_ctrl)
+
+    def desfuzzy(self):
+        #Input
+        self.fissing1_env.input['Material usage'] = self.material_usage_value
+        self.fissing1_env.input['Recicled material usage'] = self.recicled_material_value
+        self.fissing2_env.input['Water usage'] = self.water_usage_value
+        self.fissing2_env.input['Energy usage'] = self.energy_usage_value
+        self.fissing3_env.input['Waste'] = self.waste_value
+        self.fissing3_env.input['Emissions'] = self.emissions_value
+        #Crunch the numbers
+        self.fissing1_env.compute()
+        self.fissing2_env.compute()
+        self.fissing3_env.compute()
+        self.fissing4_env.input['Fis 1 env out'] = self.fissing1_env.output['FIS1_ENV']
+        self.fissing4_env.input['Fis 2 env out'] = self.fissing2_env.output['FIS2_ENV']
+        self.fissing4_env.compute()
+        self.fissing5_env.input['Fis 3 env out'] = self.fissing3_env.output['FIS3_ENV']
+        self.fissing5_env.input['Fis 4 env out'] = self.fissing4_env.output['FIS4_ENV']
+        self.fissing5_env.compute()
+
+    def run(self):
+        self.antecedent()
+        self.consequent()
+        self.rules()
+        self.desfuzzy()
+    
+        fb1={'meta':'Material Usage + Recicled Usage',
+            'result':self.fissing1_env.output['FIS1_ENV'],
+             'membership_interp':self.infer(self.fis1_env,self.fissing1_env,'FIS1_ENV')}
+        
+        fb2={'meta':'Water Usage + Energy Usage',
+            'result':self.fissing2_env.output['FIS2_ENV'],
+             'membership_interp':self.infer(self.fis2_env,self.fissing2_env,'FIS2_ENV')}
+
+        fb3={'meta':'Emissions + Waste',
+            'result':self.fissing3_env.output['FIS3_ENV'],
+             'membership_interp':self.infer(self.fis3_env,self.fissing3_env,'FIS3_ENV')}
+
+        fb4={'meta':'FB1 + FB2',
+            'result':self.fissing4_env.output['FIS4_ENV'],
+             'membership_interp':self.infer(self.fis4_env,self.fissing4_env,'FIS4_ENV')}
+        
+        fb5={'meta':'FB3 + FB4',
+            'result':self.fissing5_env.output['FIS5_ENV'],
+             'membership_interp':self.infer(self.fis5_env,self.fissing5_env,'FIS5_ENV')}
+        
+        return {'name':'Environmental',
+                'FB1':fb1,'FB2':fb2,
+                'FB3':fb3,'FB4':fb4,'FB5':fb5}
+
+    def infer(self,fbox,fboxing,name):
+        RESULT= fboxing.output[name]
+        poor=fuzz.interp_membership(fbox.universe, fbox['poor'].mf, RESULT)
+        average=fuzz.interp_membership(fbox.universe, fbox['average'].mf, RESULT)
+        good = fuzz.interp_membership(fbox.universe, fbox['good'].mf, RESULT)
+        result = {'poor':poor,'average':average,'good':good}
+        winner = np.argmax([poor,average,good])
+        result['label']=list(result.items())[winner][0]
+        return result
+
+
+if __name__=='__main__':
+    Eco=Environmental_fuzzy(1,6,4,6,5,1)
+    r=Eco.run()
+    print(r)
+
+
+    
+
---
+++ fuzzy_system/second_stage_fuzzy.py
@@ -0,0 +1,120 @@
+import numpy as np
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
+
+
+class Final_fuzzy:
+
+    def __init__(self, fbeco,fbenv,fbsoc):
+        self.range=np.arange(1,8,1)
+        self.poor=[1,1,4]
+        self.average=[2,4,6]
+        self.good=[4,7,7]
+        self.fb_economic_value = fbeco
+        self.fb_environmental_value = fbenv
+        self.fb_social_value = fbsoc
+
+    def antecedent(self):
+        self.fis_final_eco=ctrl.Antecedent(self.range, 'Fis final eco')
+        self.fis_final_env=ctrl.Antecedent(self.range, 'Fis final env')
+        self.fis_final_soc=ctrl.Antecedent(self.range, 'Fis final soc')
+
+        ##-----Membership Functions
+        #FB1
+        self.fis_final_eco['poor'] = fuzz.trimf(self.fis_final_eco.universe, self.poor)
+        self.fis_final_eco['average'] = fuzz.trimf(self.fis_final_eco.universe, self.average)
+        self.fis_final_eco['good'] = fuzz.trimf(self.fis_final_eco.universe, self.good)
+        
+        self.fis_final_env['poor'] = fuzz.trimf(self.fis_final_env.universe, self.poor)
+        self.fis_final_env['average'] = fuzz.trimf(self.fis_final_env.universe, self.average)
+        self.fis_final_env['good'] = fuzz.trimf(self.fis_final_env.universe, self.good)
+
+        #FB2
+        self.fis_final_soc['poor'] = fuzz.trimf(self.fis_final_soc.universe, self.poor)
+        self.fis_final_soc['average'] = fuzz.trimf(self.fis_final_soc.universe, self.average)
+        self.fis_final_soc['good'] = fuzz.trimf(self.fis_final_soc.universe, self.good)
+
+    def consequent(self):
+        #FIS1_SOC
+        self.fis_final = ctrl.Consequent(self.range,'FIS_FINAL')
+        self.fis_final['poor'] = fuzz.trimf(self.fis_final.universe, self.poor)
+        self.fis_final['average'] = fuzz.trimf(self.fis_final.universe, self.average)
+        self.fis_final['good'] = fuzz.trimf(self.fis_final.universe, self.good)
+    
+    def rules(self):
+        rule1_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['poor'] & self.fis_final_soc['poor'], self.fis_final['poor'])
+        rule2_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['poor'] & self.fis_final_soc['average'], self.fis_final['poor'])
+        rule3_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['poor'] & self.fis_final_soc['good'], self.fis_final['poor'])
+        rule4_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['average'] & self.fis_final_soc['poor'], self.fis_final['poor'])
+        rule5_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['average'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule6_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['average'] & self.fis_final_soc['good'], self.fis_final['average'])
+        rule7_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['good'] & self.fis_final_soc['poor'], self.fis_final['poor'])
+        rule8_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['good'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule9_final = ctrl.Rule(self.fis_final_eco['poor'] & self.fis_final_env['good'] & self.fis_final_soc['good'], self.fis_final['average'])
+
+        rule10_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['poor'] & self.fis_final_soc['poor'], self.fis_final['poor'])
+        rule11_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['poor'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule12_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['poor'] & self.fis_final_soc['good'], self.fis_final['average'])
+        rule13_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['average'] & self.fis_final_soc['poor'], self.fis_final['average'])
+        rule14_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['average'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule15_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['average'] & self.fis_final_soc['good'], self.fis_final['average'])
+        rule16_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['good'] & self.fis_final_soc['poor'], self.fis_final['average'])
+        rule17_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['good'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule18_final = ctrl.Rule(self.fis_final_eco['average'] & self.fis_final_env['good'] & self.fis_final_soc['good'], self.fis_final['average'])
+
+        rule19_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['poor'] & self.fis_final_soc['poor'], self.fis_final['poor'])
+        rule20_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['poor'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule21_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['poor'] & self.fis_final_soc['good'], self.fis_final['average'])
+        rule22_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['average'] & self.fis_final_soc['poor'], self.fis_final['average'])
+        rule23_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['average'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule24_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['average'] & self.fis_final_soc['good'], self.fis_final['average'])
+        rule25_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['good'] & self.fis_final_soc['poor'], self.fis_final['average'])
+        rule26_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['good'] & self.fis_final_soc['average'], self.fis_final['average'])
+        rule27_final = ctrl.Rule(self.fis_final_eco['good'] & self.fis_final_env['good'] & self.fis_final_soc['good'], self.fis_final['good'])
+
+
+        fis_final_ctrl = ctrl.ControlSystem([rule1_final,rule2_final,rule3_final,rule4_final,rule5_final,rule6_final,rule7_final,rule8_final,rule9_final,
+                                     rule10_final,rule11_final,rule12_final,rule13_final,rule14_final,rule15_final,rule16_final,rule17_final,rule18_final,
+                                     rule19_final,rule20_final,rule21_final,rule22_final,rule23_final,rule24_final,rule25_final,rule26_final,rule27_final])
+
+        self.fissing_final = ctrl.ControlSystemSimulation(fis_final_ctrl)
+
+    def desfuzzy(self):
+        self.fissing_final.input['Fis final eco'] = self.fb_economic_value
+        self.fissing_final.input['Fis final env'] = self.fb_environmental_value
+        self.fissing_final.input['Fis final soc'] = self.fb_social_value
+        self.fissing_final.compute()
+
+    def run(self):
+        self.antecedent()
+        self.consequent()
+        self.rules()
+        self.desfuzzy()
+    
+        fb_final={'meta':'FB_ECO + FB_ENV + FB_SOC',
+            'result':self.fissing_final.output['FIS_FINAL'],
+             'membership_interp':self.infer(self.fis_final,self.fissing_final,'FIS_FINAL')}
+
+        return {'name':'Susteinability Score',
+                'FB_FINAL':fb_final}
+
+    def infer(self,fbox,fboxing,name):
+        RESULT= fboxing.output[name]
+        poor=fuzz.interp_membership(fbox.universe, fbox['poor'].mf, RESULT)
+        average=fuzz.interp_membership(fbox.universe, fbox['average'].mf, RESULT)
+        good = fuzz.interp_membership(fbox.universe, fbox['good'].mf, RESULT)
+        result = {'poor':poor,'average':average,'good':good}
+        winner = np.argmax([poor,average,good])
+        result['label']=list(result.items())[winner][0]
+        return result
+
+
+if __name__=='__main__':
+    Eco=Final_fuzzy(5,5,5)
+    r=Eco.run()
+    print(r)
+    print(Eco)
+
+
+    
+
